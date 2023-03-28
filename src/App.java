ac97cef9a42b6371aa5e7e836f70a029f2258580
Wyatt Trinidad <wyatt.tdad@gmail.com>
2023-03-27 21:34:15 -0500

    5 numbers

--- src/App.java
+++ src/App.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
-import java.util.*;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
-    
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+        System.out.println("Enter the required size of the array. ::" );
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int myArray[] = new int [size];
+        int sum = 0;
+        int product = 1;
+        int max = myArray[0];
+        int min = myArray[0];
+        
+        System.out.println("Enter the elements of the array one by one ");
+        for (int i=0; i<size; i++){
+            myArray[i] = s.nextInt();
+            sum = sum + myArray[i];
+            product = product * myArray[i];
+            min = Math.min(min, myArray[i]);
+            max = Math.max(max, myArray[i]);
+        }
+        System.out.println("Contents of the array are: "+ Arrays.toString(myArray));
+        System.out.println("Sum of the array are: "+ sum);
+        System.out.println("Product of the array are: "+ product);
+        System.out.println("Max "+ max);
+        System.out.println("Min "+ min);
     }
 }
